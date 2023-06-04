6dce45ed05e9145aac13d815882cf407bb4a2615
mkpbd <mostofakamal1990@gmail.com>
2023-06-04 14:11:40 +0600

    union types

--- src/typescript/anotation.ts
+++ src/typescript/anotation.ts
@@ -31,5 +31,108 @@ You can also add return type annotations. Return type annotations appear after t
 
 
 function getFavoriteNumber(): number {
-    return 26;
+  return 26;
+}
+
+
+
+// Anonymous Functions
+
+const names = ["Alice", "Bob", "Eve"];
+
+// Contextual typing for function - parameter s inferred to have type string
+names.forEach(function (s) {
+  console.log(s.toUpperCase());
+});
+
+// Contextual typing also applies to arrow functions
+names.forEach((s) => {
+  console.log(s.toUpperCase());
+});
+
+
+// ============================== Object Types ===========================
+
+
+// The parameter's type annotation is an object type
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+
+
+// Optional Properties
+// Object types can also specify that some or all of their properties are optional. To do this, add a ? after the property name:
+
+function printName(obj: { first: string; last?: string }) {
+  // ...
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+
+
+
+function printNames(obj: { first: string; last?: string }) {
+  // Error - might crash if 'obj.last' wasn't provided!
+  console.log(obj?.last?.toUpperCase());
+  // 'obj.last' is possibly 'undefined'.
+  if (obj.last !== undefined) {
+    // OK
+    console.log(obj.last?.toUpperCase());
   }
+
+  // A safe alternative using modern JavaScript syntax:
+  console.log(obj.last?.toUpperCase());
+}
+
+
+//================================ Union Types ========================
+
+/**
+ * 
+ * 
+ * TypeScript’s type system allows you to build new types out of existing ones using a large variety of operators.
+ *  Now that we know how to write a few types, it’s time to start combining them in interesting ways.
+ * 
+ */
+
+function printId(id: number | string) {
+  console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+// OK
+printId("202");
+
+
+//-----------------------  TypeScript knows that only a string value will have a typeof value "string": =====================
+
+function printIds(id: number | string) {
+  if (typeof id === "string") {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
+
+///================= Another example is to use a function like Array.isArray:=====================
+
+function welcomePeople(x: string[] | string) {
+  if (Array.isArray(x)) {
+    // Here: 'x' is 'string[]'
+    console.log("Hello, " + x.join(" and "));
+  } else {
+    // Here: 'x' is 'string'
+    console.log("Welcome lone traveler " + x);
+  }
+}
+
+
+// Return type is inferred as number[] | string
+function getFirstThree(x: number[] | string) {
+  return x.slice(0, 3);
+}
