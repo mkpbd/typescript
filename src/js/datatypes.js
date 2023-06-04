620d1648ab1f43d8adb28186e681364368c8dae8
mkpbd <mostofakamal1990@gmail.com>
2023-06-04 12:51:14 +0600

    data types

---
+++ src/js/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";
+// string 
+let firstName;
+firstName = "mostofa kamal passa";
+// number 
+let integerNumber;
+integerNumber = 40;
+// integerNumber = '';
+// boolean 
+let booleans;
+booleans = true;
+booleans = false;
+// booleans = 1;
+// null 
+let nullbles;
+// nullbles = "abc"
+// undefined
+let undefinedbles;
+undefinedbles = undefined;
+// undefinedbles = '';
+let symbols;
+symbols = Symbol();
+// symbols = 'hello';
+// ---------------- Array Types  ---------------------
+let stringArrayes;
+stringArrayes = ["hello", "bangla", "desh"];
+let numberArrayes;
+numberArrayes = [4, 5, 6, 7, 8, 9];
+let mixedArray;
+mixedArray = ["hello", 1, 3, 4, 5, null, undefined, Symbol()];
---
+++ src/typescript/datatypes.ts
@@ -0,0 +1,54 @@
+// string 
+
+let firstName : string ;
+
+firstName = "mostofa kamal passa";
+
+
+// number 
+
+let integerNumber : number;
+integerNumber = 40;
+
+// integerNumber = '';
+
+// boolean 
+
+let booleans : boolean;
+booleans = true;
+booleans = false;
+
+// booleans = 1;
+
+
+// null 
+
+let nullbles : null;
+// nullbles = "abc"
+
+// undefined
+
+let undefinedbles : undefined;
+
+undefinedbles = undefined;
+// undefinedbles = '';
+
+let symbols : symbol;
+
+symbols =  Symbol();
+// symbols = 'hello';
+
+
+// ---------------- Array Types  ---------------------
+let stringArrayes : string[];
+
+stringArrayes = ["hello", "bangla", "desh"];
+
+let numberArrayes : number[];
+numberArrayes = [4,5,6,7,8,9];
+
+let mixedArray : any[];
+
+mixedArray = ["hello", 1, 3,4,5, null, undefined, Symbol()]
+
+
