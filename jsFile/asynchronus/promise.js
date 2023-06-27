1216ccdb45263c2851a9000e81c516e98e21217f
mostofa kamal <mostofakamal1990@gmail.com>
2023-06-27 17:22:54 +0600

    promise js

--- jsFile/asynchronus/promise.js
+++ jsFile/asynchronus/promise.js
@@ -1,33 +1,38 @@
 "use strict";
-function appendAndReadPromise(path, data) {
-    return appendPromise(path, data)
-        .then(() => readPromise(path))
-        .catch(error => console.error(error));
-}
-function appendAndRead(path, data, cb) {
-    //appendFile(path, data, error => {
-    //  if (error) {
-    // return cb(error, null)
-    //   }
-    //  readFile(path, (error, result) => {
-    //   if (error) {
-    //   //       return cb(error, null)
-    //      }
-    //       cb(null, result)
-    //    })
-    // })
-    //}
-    function readFilePromise(path) {
-        return new Promise((resolve, reject) => {
-            readFile(path, (error, result) => {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    // resolve(result)
-                }
-            });
-        });
-    }
-    function readFile(path, callback) { }
-}
+// function appendAndReadPromise(path: string, data: string): Promise<string> {
+//     return appendPromise(path, data)
+//         .then(() => readPromise(path))
+//         .catch(error => console.error(error))
+// }
+// function appendAndRead(
+//     path: string,
+//     data: string,
+//     cb: (error: Error | null, result: string | null) => void
+// ) {
+//appendFile(path, data, error => {
+//  if (error) {
+// return cb(error, null)
+//   }
+//  readFile(path, (error, result) => {
+//   if (error) {
+//   //       return cb(error, null)
+//      }
+//       cb(null, result)
+//    })
+// })
+//}
+// function readFilePromise(path: string): Promise<string> {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, (error, result) => {
+//             if (error) {
+//                 reject(error)
+//             } else {
+//                 // resolve(result)
+//             }
+//         })
+//     })
+// }
+// function readFile(
+//     path: string,
+//     callback: (err: Error | null, data: string | null | number | PromiseLike<string>) => void
+// ): void { }
