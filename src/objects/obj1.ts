3c603a860d0aedb9c2a915a8656efa74e4e96a8f
mostofa kamal <mostofakamal1990@gmail.com>
2023-06-28 14:52:23 +0600

    intersection data

--- src/objects/obj1.ts
+++ src/objects/obj1.ts
@@ -106,3 +106,119 @@ const poemMatch: Poem = {
     },
     name: "Lady Lazarus",
 };
+
+//============== Explicit Object-Type Unions =========
+
+type PoemWithPages = {
+    name: string;
+    pages: number;
+};
+type PoemWithRhymes = {
+    name: string;
+    rhymes: boolean;
+};
+type Poem1 = PoemWithPages | PoemWithRhymes;
+const poem: Poem1 =
+    Math.random() > 0.5
+        ? {
+            name: "The Double Image",
+            pages: 7,
+        }
+        : {
+            name: "Her Kind",
+            rhymes: true,
+        };
+
+type PoemWithPages1 = {
+    name: string;
+    pages: number;
+    type: "pages";
+};
+
+type PoemWithRhymes1 = { name: string; rhymes: boolean; type: "rhymes" };
+type Poem2 = PoemWithPages1 | PoemWithRhymes1;
+const poem1: Poem2 =
+    Math.random() > 0.5
+        ? {
+            name: "The Double Image",
+            pages: 7,
+            type: "pages",
+        }
+        : {
+            name: "Her Kind",
+            rhymes: true,
+            type: "rhymes",
+        };
+// if (poem.type === "pages") {
+//     console.log(`It's got pages: ${poem.pages}`);
+//     // Ok
+// } else {
+//     console.log(`It rhymes: ${poem.rhymes}`);
+// }
+// poem2.type;
+//           // Type: 'pages' | 'rhymes'
+
+//====================== Intersection Types ====================
+
+
+type Artwork = {
+    genre: string;
+    name: string;
+};
+type Writing = {
+    pages: number;
+    name: string;
+};
+
+type WrittenArt = Artwork | Writing
+// Equivalent to:
+// {
+// genre: string;
+// name: string;
+// pages: number;
+// }
+
+
+type ShortPoem = {
+    author: string;
+} & (
+        | {
+            kigo: string;
+            type: "haiku";
+        }
+        | {
+            meter: number;
+            type: "villanelle";
+        }
+    );
+// Ok
+const morningGlory: ShortPoem = {
+    author: "Fukuda Chiyo-ni",
+    kigo: "Morning Glory",
+    type: "haiku",
+};
+// const oneArt1: ShortPoem = {
+//     author: "Elizabeth Bishop",
+//     meter: 'Medium',
+
+//     type: "villanelle",
+
+// };
+
+
+type ShortPoemBase = {
+    author: string;
+};
+type Haiku = ShortPoemBase & {
+    kigo: string;
+    type: "haiku";
+};
+type Villanelle = ShortPoemBase & {
+    meter: number;
+    type: "villanelle";
+};
+type ShortPoem3 = Haiku | Villanelle;
+//   const oneArt2: ShortPoem3 = {
+//     author: "Elizabeth Bishop",
+//     type: "villanelle",
+//   };
