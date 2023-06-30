504bbff99952c2ff69b5dfaabf4a7e229316c9a5
mostofa kamal <mostofakamal1990@gmail.com>
2023-06-30 14:15:54 +0600

    Decorators

--- src/syntextExtension/decorators.ts
+++ src/syntextExtension/decorators.ts
@@ -8,13 +8,124 @@ function logOnCall(target: any, key: string, descriptor: PropertyDescriptor) {
 }
 
 class Greeter1 {
-    //  @logOnCall
+    @logOnCall
     greet(message: string) {
         console.log(`[greet] Hello, ${message}!`);
     }
 }
 new Greeter1().greet("you");
-  // Output log:
-  // "[logOnCall] I am decorating", "Greeter"
-  // "[descriptor.value] Calling 'greet' with:", "you"
-  // "[greet] Hello, you!"
+// Output log:
+// "[logOnCall] I am decorating", "Greeter"
+// "[descriptor.value] Calling 'greet' with:", "you"
+// "[greet] Hello, you!"
+
+
+
+
+function first() {
+    console.log("first(): factory evaluated");
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log("first(): called");
+    };
+}
+
+function second() {
+    console.log("second(): factory evaluated");
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log("second(): called");
+    };
+}
+
+class ExampleClassDecorator {
+    @first()
+    @second()
+    method() { }
+}
+
+
+// let decoratorRe = new ExampleClassDecorator();
+
+function sealed(constructor: Function) {
+    Object.seal(constructor);
+    Object.seal(constructor.prototype);
+}
+
+//========= Class Decorator =================================
+@sealed
+class BugReport {
+    type = "report";
+    title: string;
+
+    constructor(t: string) {
+        this.title = t;
+    }
+}
+
+//================ Methods decorators =================================
+
+function enumerable(value: boolean) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        descriptor.enumerable = value;
+    };
+}
+class Greeter3 {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    @enumerable(false)
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+//================= Accessor Decorators 
+function configurable(value: boolean) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        descriptor.configurable = value;
+    };
+}
+class Point {
+    private _x: number;
+    private _y: number;
+    constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+
+    @configurable(false)
+    get x() {
+        return this._x;
+    }
+
+    @configurable(false)
+    get y() {
+        return this._y;
+    }
+}
+
+//==================== Property Decorators ==================
+
+import "reflect-metadata";
+const formatMetadataKey = Symbol("format");
+function format(formatString: string) {
+    //return Reflect.metadata(formatMetadataKey, formatString);
+    return formatString.trim();
+}
+function getFormat(target: any, propertyKey: string) {
+    //return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
+    return target;
+}
+
+class Greeter5 {
+    // @format("Hello, %s")
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        let formatString = getFormat(this, "greeting");
+        return formatString.replace("%s", this.greeting);
+    }
+}
